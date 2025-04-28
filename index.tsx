75bea864927a0614b1d1f6227f2c35e7aac6301e
tuda-1117 <h.tsuda@obora-pri.co.jp>
2025-04-28 11:41:01 +0900

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,80 @@
+# テクニカルガイド クリアファイルデータ作成時チェックポイント
+
+テクニカルガイド | オリジナルクリアファイルの印刷・通販はボラネット
+
+オリジナルクリアファイルの印刷・通販はボラネット > テクニカルガイド
+
+## ボラネットのデータ作成方法は下記の３種類からお選びいただけます
+
+### ● illustrator もしくは Photoshop でのデータ作成
+
+すべての商品に対応している作成方法です。
+[テンプレートダウンロードページ](https://www.bora-net.com/beginner/list.html)からテンプレートをダウンロードいただき、
+本ページのアプリケーションごとのチェックポイントやテンプレートに同梱されているマニュアルをご参照いただきながらデータの作成をお願いします。
+
+### ● Office ソフトでのデータ作成
+
+一部の商品では、Office ソフトでも作成いただけます。
+Office ソフトをご利用の場合は、「[Microsoft Office のご利用について](/office/index.html)」のページにて詳細をご確認ください。
+
+### ● WEB でデザインでのデータ作成
+
+一部の商品では、当社ホームページ内で簡単にデザイン制作ができる「WEB でデザイン」をご利用いただきデータ作成いただけます。
+写真などをアップロードするだけで簡単にデータ作成が可能な、無料で使えるデザインテンプレートも多数ご用意しています。
+WEB でデザインをご利用の場合は、「
+[WEB でデザインについて詳細はこちら](/index_w2p.html)
+」専用ページにて詳細をご確認ください。
+
+## illustrator のチェクポイント
+
+- [トンボについて](illustrator/tonbo.html)
+- [塗り足しについて](illustrator/nuri.html)
+- [カラーモードについて](illustrator/colormode.html)
+- [レイヤーについて](illustrator/layer.html)
+- [配置画像について](illustrator/picture.html)
+- [文字のアウトライン化について](illustrator/font.html)
+- [線・塗りの設定について](illustrator/line.html)
+- [スミベタ・リッチブラックについて](illustrator/sumibeta.html)
+- [オーバープリント（スミノセ）について](illustrator/print.html)
+- [各種効果を使用する際の注意](illustrator/kouka.html)
+- [保存について](illustrator/save.html)
+
+[PDF ダウンロード](pdf/illustrator.pdf)チェックポイントを印刷できるようになっています。
+右ボタンから PDF をダウンロードしてご利用ください。
+
+## Photoshop のチェクポイント
+
+- [解像度について](photoshop/picture.html)
+- [カラーモードについて](photoshop/colormode.html)
+- [レイヤーについて](photoshop/layer.html)
+- [保存について](photoshop/save.html)
+
+[PDF ダウンロード](pdf/photoshop.pdf)チェックポイントを印刷できるようになっています。
+右ボタンから PDF をダウンロードしてご利用ください。
+
+## 白版の必要性と作り方
+
+- [白版の必要性](white/need.html)
+- [白版作成パターン](white/pattern.html)
+- [イラストレーターによる作成](white/use_illustrator.html)
+- [フォトショップによる作成](white/use_photoshop.html)
+- [白版作成のその他のポイント](white/point.html)
+
+[PDF ダウンロード](pdf/hakuban.pdf)チェックポイントを印刷できるようになっています。
+右ボタンから PDF をダウンロードしてご利用ください。
+
+## よくある不備データ
+
+- [完全データ入稿が基本です](date/comp.html)
+- [テンプレートに作っていない](date/template.html)
+- [クリアファイルの白版が作っていない](date/clearfile.html)
+- [塗り足しがない](date/nuri.html)
+- [文字のアウトライン化がしていない](date/outline.html)
+- [解像度が低い](date/granularity.html)
+- [配置画像のリンクが切れている](date/link.html)
+- [スミノセの失敗](date/suminose.html)
+
+[PDF ダウンロード](pdf/fubidata.pdf)チェックポイントを印刷できるようになっています。
+右ボタンから PDF をダウンロードしてご利用ください。
+
+オリジナルクリアファイルの印刷・通販はボラネット > テクニカルガイド
